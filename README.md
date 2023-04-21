faa3888c5efca76dcf76e1541a28dc5d037858db
MNIKIEMA <nikiemamahamadi01@gmail.com>
2023-04-21 19:14:55 +0200

    "add data"

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # bert-intent-detection
-Intent detection and slot filling with BERT
+Intent detection and slot filling with BERT inspired from [Benoit Favre](https://pageperso.lis-lab.fr/benoit.favre/pstaln/08_contextual-embeddings.html)
---
+++ fetch_data.py
@@ -0,0 +1,14 @@
+from urllib.request import urlretrieve
+from pathlib import Path
+
+
+SNIPS_DATA_BASE_URL = (
+    "https://github.com/MNIKIEMA/slot_filling_and_intent_detection_of_SLU/tree/master/data/snips/"
+)
+root = Path("./snips")
+for filename in ["train", "valid", "test", "vocab.intent", "vocab.slot"]:
+    path = Path(r"snips-data/"+filename)
+    print(path)
+    if not path.exists():
+        print(f"Downloading {filename}...")
+        urlretrieve(SNIPS_DATA_BASE_URL + filename + "?raw=true", path)
